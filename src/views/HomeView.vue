aac9f32a3a9683bb21f4b7fba99d3717e777b31b
oludavid1 <oluwaseundavid587@gmail.com>
2023-08-18 12:41:13 +0100

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <v-app>
+    <NavbarPage class="index" />
+    <DashBoard />
+    <FooterPage />
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import NavbarPage from '../components/NavbarPage'
+  import DashBoard from '../components/DashBoard'
+  import FooterPage from '../components/FooterPage'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'HomeView',
+
+    components: {
+      NavbarPage,
+      DashBoard,
+      FooterPage
+    },
   }
-}
 </script>
+
+<style scoped>
+.index{
+  z-index: 9999;
+}
+</style>
